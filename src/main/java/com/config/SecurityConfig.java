69a491fb27bc790c9afed6e0275d27a397761694
Matheus Camargo <matheuscamargotech@gmail.com>
2025-10-20 20:37:36 -0300

    security config class

---
+++ src/main/java/com/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.camargo.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+    // Desabilita o CSRF, APIs REST usam tokens (como JWT) para segurança, e não dependem de cookies de sessão para prevenir ataques.
+    .csrf(csrf -> csrf.disable())
+
+    // Define a API como stateless (sem estado), usando apenas tokens
+    .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    // Define as regras de autorização para os endpoints
+    .authorizeHttpRequests(authorize -> authorize
+
+    // Permite acesso irrestrito para todas as requisições GET em /api/products.
+    .requestMatchers("/api/products").permitAll()
+    .requestMatchers("/api/products/*").permitAll()
+
+    .requestMatchers(HttpMethod.POST, "/api/products").hasRole("ADMIN")
+    .requestMatchers(HttpMethod.PUT, "/api/products/*").hasRole("ADMIN")
+    .requestMatchers(HttpMethod.DELETE, "/api/products/*").hasRole("ADMIN")
+
+    // O que sobrar (se houver outro endpoint)
+    .anyRequest().authenticated()
+    );
+
+    // Retorna o objeto de configuração montado
+    return http.build();
+    }
+}
